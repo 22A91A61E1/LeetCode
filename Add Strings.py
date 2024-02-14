b63dac1c7423868e3a22440cb77f4df871f85122
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2024-02-14 18:53:40 +0530

    Add Strings.py

---
+++ Add Strings.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+       
+        a=int(num1)
+        b=int(num2)
+
+        c=a+b
+        d=str(c)
+        return d
